2b12a89acd1a7c424ed694c1e9796b7750ffe6bd
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2023-07-06 23:07:16 -0300

    Exercise 1.6

---
+++ src/tests/1-using-promises/1.6/exercise.test.ts
@@ -0,0 +1,44 @@
+import makeExercise from "../../../content/1-using-promises/1.6/exercise";
+import { makeGraphExerciseTestCase } from "../../../lib/graphExercise/graphExerciseTestCase";
+
+const graphExerciseTestCase = makeGraphExerciseTestCase({ makeExercise });
+
+graphExerciseTestCase("1.6", [
+  { created: ["A"] },
+  { resolved: "A", created: ["B", "C", "D"] },
+  { resolved: "B", created: [] },
+  { resolved: "C", created: ["E"] },
+  { resolved: "D", created: ["F"] },
+  { resolved: "E", created: [] },
+  { resolved: "F", created: [] },
+]);
+
+graphExerciseTestCase("1.6", [
+  { created: ["A"] },
+  { resolved: "A", created: ["B", "C", "D"] },
+  { resolved: "C", created: [] },
+  { resolved: "B", created: ["E"] },
+  { resolved: "D", created: ["F"] },
+  { resolved: "E", created: [] },
+  { resolved: "F", created: [] },
+]);
+
+graphExerciseTestCase("1.6", [
+  { created: ["A"] },
+  { resolved: "A", created: ["B", "C", "D"] },
+  { resolved: "D", created: [] },
+  { resolved: "B", created: [] },
+  { resolved: "C", created: ["E", "F"] },
+  { resolved: "E", created: [] },
+  { resolved: "F", created: [] },
+]);
+
+graphExerciseTestCase("1.6", [
+  { created: ["A"] },
+  { resolved: "A", created: ["B", "C", "D"] },
+  { resolved: "D", created: [] },
+  { resolved: "B", created: [] },
+  { resolved: "C", created: ["E", "F"] },
+  { resolved: "F", created: [] },
+  { resolved: "E", created: [] },
+]);
