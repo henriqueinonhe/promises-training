a7667ab567cd67ef6f6ce797ded210a182379c71
Henrique Inonhe <henriqueinonhe@gmail.com>
2023-07-09 19:10:27 -0300

    Promifisy impl

---
+++ src/content/foundation/promisify.ts
@@ -0,0 +1,24 @@
+type FunctionWithCallback<T, Args extends Array<unknown>> = (
+  ...params: [...Args, Callback<T>]
+) => void;
+
+type Callback<T> = (err: unknown, data: T) => void;
+
+type PromisifiedFunction<T, Args extends Array<unknown>> = (
+  ...args: Args
+) => Promise<T>;
+
+export default <T, Args extends Array<unknown>>(
+    fn: FunctionWithCallback<T, Args>
+  ): PromisifiedFunction<T, Args> =>
+  (...args) =>
+    new Promise((resolve, reject) => {
+      fn(...args, (err: unknown, data: T) => {
+        if (err !== undefined) {
+          reject(err);
+          return;
+        }
+
+        resolve(data);
+      });
+    });
