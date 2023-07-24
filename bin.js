5d330f920dd76ed4da44bbf814cf8d2b05600d2a
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2023-07-24 09:50:13 -0300

    Implement setup

--- bin.js
+++ bin.js
@@ -1,10 +1,33 @@
 #!/usr/bin/env node
 
 const { execSync } = require("node:child_process");
+const { cp, rm, rename, readdir } = require("node:fs/promises");
 
 const run = (command) => execSync(command, { stdio: "inherit" });
 
-run(`npm install`);
-run(`npm install`, {
-  cwd: `./src/lib/graphExercise/ui`,
-});
+const main = async () => {
+  console.log(__dirname);
+
+  // run(`npm install`);
+  // run(`npm install`, {
+  //   cwd: `./src/lib/graphExercise/ui`,
+  // });
+  // await Promise.all(
+  //   ["graph", "concrete", "foundation"].map(replaceWithTemplates)
+  // );
+};
+
+const replaceWithTemplates = async (category) => {
+  const exercises = await readdir(`./src/exercises/${category}`);
+
+  await Promise.all(
+    exercises.map(async (exercise) => {
+      const exercisePath = `./src/exercises/${category}/${exercise}/exercise.ts`;
+      const exerciseTemplatePath = `./src/exercises/${category}/${exercise}/template.ts`;
+      await rm(exerciseTemplatePath);
+      await rename(exercisePath, exerciseTemplatePath);
+    })
+  );
+};
+
+main();
