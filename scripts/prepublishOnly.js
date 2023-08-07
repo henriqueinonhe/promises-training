c4fabc7ebe3fb338915f2de1457c770999f3418f
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2023-08-07 19:56:47 -0300

    Change publishing process to do most pre processing BEFORE publishing

--- scripts/prepublishOnly.js
+++ scripts/prepublishOnly.js
@@ -1,7 +1,24 @@
-const { rename } = require("node:fs/promises");
+const { rm, rename, readdir } = require("node:fs/promises");
 
 const main = async () => {
-  rename("./.gitignore", "./gitignore");
+  await rename("./.gitignore", "./gitignore");
+
+  await Promise.all(
+    ["graph", "concrete", "foundation"].map(replaceWithTemplates)
+  );
+};
+
+const replaceWithTemplates = async (category) => {
+  const exercises = await readdir(`./src/exercises/${category}`);
+
+  await Promise.all(
+    exercises.map(async (exercise) => {
+      const exercisePath = `./src/exercises/${category}/${exercise}/exercise.ts`;
+      const exerciseTemplatePath = `./src/exercises/${category}/${exercise}/template.ts`;
+      await rm(exercisePath);
+      await rename(exerciseTemplatePath, exercisePath);
+    })
+  );
 };
 
 main();
