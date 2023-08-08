e782b57e7276435af4c7095b74fbb7eba1f7d470
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2023-08-08 17:01:38 -0300

    Redo parallel max concurrecy tests

--- src/tests/concrete/parallelMaxConcurrency.test.ts
+++ src/tests/concrete/parallelMaxConcurrency.test.ts
@@ -1,75 +1,140 @@
-import { expect, it, vi } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { promiseWithResolvers } from "../../lib/promiseWithResolvers";
 import parallelMaxConcurrencyExercise from "../../exercises/concrete/parallelMaxConcurrency/exercise";
 import { waitForPromises } from "../../lib/waitForPromises";
 
-const setup = () => {
-  const handlers = Array.from({ length: 8 }).map(() =>
-    promiseWithResolvers<string>()
-  );
+describe("When the list has 8 elements", () => {
+  const setup = async () => {
+    const results = Array.from({ length: 8 }).map(
+      (_, index) => `Result${index + 1}`
+    );
 
-  const postData = handlers.reduce(
-    (fn, { promise }) => fn.mockReturnValueOnce(promise),
-    vi.fn()
-  );
+    const handlers = results.map((result) => {
+      const { promise, resolver } = promiseWithResolvers<string>();
 
-  const parallelMaxConcurrency = parallelMaxConcurrencyExercise({
-    postData,
+      return {
+        promise,
+        resolver: () => resolver(result),
+      };
+    });
+
+    const postData = handlers.reduce(
+      (fn, { promise }) => fn.mockReturnValueOnce(promise),
+      vi.fn()
+    );
+
+    const parallelChunks = parallelMaxConcurrencyExercise({
+      postData,
+    });
+
+    const parameters = handlers.map((_, index) => `Data${index + 1}`);
+
+    const promise = parallelChunks(parameters);
+
+    await waitForPromises();
+
+    return {
+      postData,
+      handlers,
+      promise,
+      parameters,
+      results,
+    };
+  };
+
+  it("First 5 requests are made in parallel", async () => {
+    const { postData, parameters } = await setup();
+
+    expect(postData).toHaveBeenCalledTimes(5);
+    parameters.slice(0, 5).forEach((parameter) => {
+      expect(postData).toHaveBeenCalledWith(parameter);
+    });
   });
 
-  const parameters = handlers.map((_, index) => `Data${index}`);
+  describe("And third request resolves", () => {
+    const secondSetup = async () => {
+      const previousSetupReturnValue = await setup();
 
-  const promise = parallelMaxConcurrency(parameters);
+      const { handlers } = previousSetupReturnValue;
 
-  return {
-    postData,
-    handlers,
-    parallelMaxConcurrency,
-    promise,
-    parameters,
-  };
-};
+      handlers[2].resolver();
 
-it("First 5 requests are made in parallel", async () => {
-  const { postData } = setup();
+      await waitForPromises();
 
-  await waitForPromises();
+      return previousSetupReturnValue;
+    };
 
-  expect(postData).toHaveBeenCalledTimes(5);
-  expect(postData).toHaveBeenCalledWith("Data0");
-  expect(postData).toHaveBeenCalledWith("Data1");
-  expect(postData).toHaveBeenCalledWith("Data2");
-  expect(postData).toHaveBeenCalledWith("Data3");
-  expect(postData).toHaveBeenCalledWith("Data4");
-});
+    it("Sixth request is made", async () => {
+      const { postData, parameters } = await secondSetup();
 
-it("As requests finish, others are made, in sequence", async () => {
-  const { handlers, postData } = setup();
+      expect(postData).toHaveBeenCalledTimes(6);
+      expect(postData).toHaveBeenCalledWith(parameters[5]);
+    });
 
-  handlers[0].resolver("Data5");
-  await waitForPromises();
-  expect(postData).toHaveBeenCalledTimes(6);
-  expect(postData).toHaveBeenCalledWith("Data5");
+    describe("And first request resolves", () => {
+      const thirdSetup = async () => {
+        const previousSetupReturnValue = await secondSetup();
 
-  handlers[3].resolver("Data6");
-  await waitForPromises();
-  expect(postData).toHaveBeenCalledTimes(7);
-  expect(postData).toHaveBeenCalledWith("Data6");
+        const { handlers } = previousSetupReturnValue;
 
-  handlers[4].resolver("Data7");
-  await waitForPromises();
-  expect(postData).toHaveBeenCalledTimes(8);
-  expect(postData).toHaveBeenCalledWith("Data7");
-});
+        handlers[0].resolver();
 
-it("Returns the results of all requests in sequence", async () => {
-  const { handlers, promise } = setup();
+        await waitForPromises();
 
-  handlers.forEach((handler, index) => handler.resolver(`Result${index}`));
+        return previousSetupReturnValue;
+      };
 
-  const results = await promise;
+      it("Seventh request is made", async () => {
+        const { postData, parameters } = await thirdSetup();
 
-  expect(results).toEqual(
-    await Promise.all(handlers.map(({ promise }) => promise))
-  );
+        expect(postData).toHaveBeenCalledTimes(7);
+        expect(postData).toHaveBeenCalledWith(parameters[6]);
+      });
+
+      describe("And fourth request resolves", () => {
+        const fourthSetup = async () => {
+          const previousSetupReturnValue = await thirdSetup();
+
+          const { handlers } = previousSetupReturnValue;
+
+          handlers[3].resolver();
+
+          await waitForPromises();
+
+          return previousSetupReturnValue;
+        };
+
+        it("Eighth request is made", async () => {
+          const { postData, parameters } = await fourthSetup();
+
+          expect(postData).toHaveBeenCalledTimes(8);
+          expect(postData).toHaveBeenCalledWith(parameters[7]);
+        });
+
+        describe("And other requests resolve", () => {
+          const fifthSetup = async () => {
+            const previousSetupReturnValue = await fourthSetup();
+
+            const { handlers } = previousSetupReturnValue;
+
+            handlers[1].resolver();
+            handlers[4].resolver();
+            handlers[5].resolver();
+            handlers[6].resolver();
+            handlers[7].resolver();
+
+            await waitForPromises();
+
+            return previousSetupReturnValue;
+          };
+
+          it("Returns results in order", async () => {
+            const { promise, results } = await fifthSetup();
+
+            expect(await promise).toEqual(results);
+          });
+        });
+      });
+    });
+  });
 });
